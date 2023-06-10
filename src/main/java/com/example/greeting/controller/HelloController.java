fca66ba25e1dd34db7bbb85e8fbe31efb51d45d7
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-10 12:12:04 +0900

    Hello Worldの実装

---
+++ src/main/java/com/example/greeting/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.greeting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/hello")
+    private String hello() {
+        return "/index.html";
+    }
+}
