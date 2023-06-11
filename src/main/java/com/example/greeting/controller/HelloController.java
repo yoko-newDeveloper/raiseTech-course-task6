16cedb3dcade68fee4c5e83f9f3352c19bb8ada4
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-11 22:04:56 +0900

    index.html：L10indexに修正

--- src/main/java/com/example/greeting/controller/HelloController.java
+++ src/main/java/com/example/greeting/controller/HelloController.java
@@ -3,11 +3,10 @@ package com.example.greeting.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     private String hello() {
-        return "/index";
+        return "index";
     }
 }
