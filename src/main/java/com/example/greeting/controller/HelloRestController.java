52c730fb2b9c4ad104afa8cb33878d328b1eea6a
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-18 16:52:10 +0900

    @RestControllerを使用し、hello worldをレスポンスとして返す

--- src/main/java/com/example/greeting/controller/HelloRestController.java
+++ src/main/java/com/example/greeting/controller/HelloRestController.java
@@ -1,2 +1,12 @@
-package com.example.greeting.controller;public class HelloRestController {
+package com.example.greeting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @GetMapping("/hello-rest")
+    public String hello() {
+        return "hello world";
+    }
 }
